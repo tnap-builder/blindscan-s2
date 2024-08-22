d4704fe65230f7346b1349e1792cf044350469f8
tnap-builder <79226521+tnap-builder@users.noreply.github.com>
2024-08-22 17:12:58 -0400

    Add files via upload

--- blindscan-s2.c
+++ blindscan-s2.c
@@ -393,16 +393,6 @@ void blindscan (int startfreq, int endfreq, int symrate,
 				getinfo(fefd, lof, verbose);
 				usleep(100);
 				getinfo(fefd, lof, verbose);
-				usleep(100);
-				getinfo(fefd, lof, verbose);
-				usleep(100);
-				getinfo(fefd, lof, verbose);
-				usleep(100);
-				getinfo(fefd, lof, verbose);
-				usleep(100);
-				getinfo(fefd, lof, verbose);
-				usleep(100);
-				getinfo(fefd, lof, verbose);
 				//printf("usleep(s)-End \n");
                                 if (ioctl(fefd, FE_READ_STATUS, &status) == -1) {
                                         perror("FE_READ_STATUS failed");
