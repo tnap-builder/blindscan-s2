209f4d339f21e06061855c336449062ce08c9e16
tnap-builder <79226521+tnap-builder@users.noreply.github.com>
2024-08-23 20:55:49 -0400

    Add files via upload

--- blindscan-s2.c
+++ blindscan-s2.c
@@ -405,7 +405,7 @@ void blindscan (int startfreq, int endfreq, int symrate,
 
 				//FILE *fptr;
 				//fptr = fopen("/media/usb/test.txt", "a");
-				perror("\n tune = fefd %d, f %d, symrate %d, polarity %d, fec %d, delsys %d, tone %d, verbose %d", fefd, f, symrate, polarity, fec, delsys, tone, verbose);
+				printf("\n tune = fefd %d, f %d, symrate %d, polarity %d, fec %d, delsys %d, tone %d, verbose %d", fefd, f, symrate, polarity, fec, delsys, tone, verbose);
 				if (ioctl(fefd, FE_READ_STATUS, &status) == -1) {
 					perror("FE_READ_STATUS failed");
 				}
